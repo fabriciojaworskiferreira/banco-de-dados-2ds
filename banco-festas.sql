6ca32ae81eb125767c5a03b4ad9b2915e73ae4ae
fabriciojaworskiferreira <fabricio.jaworski.ferreira@escola.pr.gov.br>
2025-04-22 21:49:04 -0300

    Create banco-festas.sql

---
+++ banco-festas.sql
@@ -0,0 +1,31 @@
+
+CREATE TABLE cliente 
+( 
+); 
+
+CREATE TABLE festa 
+( 
+); 
+
+CREATE TABLE pagamento 
+( 
+); 
+
+CREATE TABLE pix 
+( 
+ codigo pix INT,  
+); 
+
+CREATE TABLE cartãodecredito 
+( 
+ endereço INT,  
+ cpf INT,  
+ nome do cartao INT,  
+); 
+
+CREATE TABLE boleto 
+( 
+ vencimento INT,  
+ cod barras INT,  
+); 
+
